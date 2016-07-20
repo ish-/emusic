7f418cde9ad302146fbca20a18a594c574460aaa
ISh- <imwaves@gmail.com>
2016-07-20 16:16:42 +0300

    Added: collapsed console to VK.request when 'development'

--- src/services/vk.request.js
+++ src/services/vk.request.js
@@ -2,7 +2,11 @@ export default function request (method, opts) {
   return new Promise((resolve, reject) => {
     VK.api(method, opts, function (d) {
       d = d.hasOwnProperty('response') ? d.response : d
-      console.log('API request: ', method, opts, d)
+      if (process.env.NODE_ENV === 'development') {
+        console.groupCollapsed('API request: ' + method, opts)
+        console.log('API request: ', d)
+        console.groupEnd()
+      }
       resolve(d)
     })
   })
