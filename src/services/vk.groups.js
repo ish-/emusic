d3c52bf47535a276f054cb64e4e9136515f00178
ISh- <imwaves@gmail.com>
2016-07-21 13:55:30 +0300

    Added: new vk.groups and foreign key

--- src/services/vk.groups.js
+++ src/services/vk.groups.js
@@ -3,64 +3,839 @@ const genres = [
     name: 'Electronic',
     groups: [
       {
-        name: 'vaporwave',
-        id: 64858245
+        id: 27122967,
+        name: "Ambient"
       }, {
-        name: 'drone',
-        id: 34125866
+        id: 33837896,
+        name: "Minimal"
       }, {
-        name: 'psybient',
-        id: 29583317
+        id: 55882119,
+        name: "Illbient"
       }, {
-        name: 'future garage',
-        id: 25247340
+        id: 35129148,
+        name: "Downtempo"
       }, {
-        name: 'ambient',
-        id: 27122967
+        id: 27683540,
+        name: "Trip-Hop"
       }, {
-        name: 'dub techno',
-        id: 55120308
+        id: 29583317,
+        name: "Psybient, Psychill"
       }, {
-        name: 'ritual',
-        id: 36286213
+        id: 64858245,
+        name: "Vaporwave"
       }, {
-        name: 'glitch',
-        id: 44955915
+        id: 35993756,
+        name: "New Age"
       }, {
-        name: 'minimal',
-        id: 33837896
-      },
+        id: 26933124,
+        name: "Lounge"
+      }, {
+        id: 34125866,
+        name: "Drone"
+      }, {
+        id: 36286213,
+        name: "Ritual"
+      }, {
+        id: 62271339,
+        name: "Bass"
+      }, {
+        id: 26134512,
+        name: "Drum & Bass"
+      }, {
+        id: 93368336,
+        name: "Intelligent Drum & Bass"
+      }, {
+        id: 104515819,
+        name: "Juke & Footwork"
+      }, {
+        id: 52278698,
+        name: "Neurofunk"
+      }, {
+        id: 32465350,
+        name: "UK Garage / Dubstep"
+      }, {
+        id: 37682149,
+        name: "Wonky"
+      }, {
+        id: 119425878,
+        name: "Grim"
+      }, {
+        id: 40952094,
+        name: "Breakbeat"
+      }, {
+        id: 28270195,
+        name: "Breaks"
+      }, {
+        id: 62625657,
+        name: "Classicore"
+      }, {
+        id: 29740811,
+        name: "Breakcore"
+      }, {
+        id: 41640789,
+        name: "Drumfunk"
+      }, {
+        id: 37615994,
+        name: "Jungle"
+      }, {
+        id: 50475680,
+        name: "Electro"
+      }, {
+        id: 29431881,
+        name: "Electropop"
+      }, {
+        id: 33391700,
+        name: "Fidget"
+      }, {
+        id: 30619123,
+        name: "Synthpop"
+      }, {
+        id: 107881046,
+        name: "New Beat"
+      }, {
+        id: 50867913,
+        name: "Coldwave"
+      }, {
+        id: 34724138,
+        name: "Darkwave"
+      }, {
+        id: 117087282,
+        name: "Retrowave"
+      }, {
+        id: 58267631,
+        name: "Retrowave (архив)"
+      }, {
+        id: 25611523,
+        name: "Futurepop"
+      }, {
+        id: 35187148,
+        name: "Traditional Electronic"
+      }, {
+        id: 86905866,
+        name: "Progressive Electronic"
+      }, {
+        id: 25616872,
+        name: "IDM"
+      }, {
+        id: 44955915,
+        name: "Glitch"
+      }, {
+        id: 37682149,
+        name: "Wonky"
+      }, {
+        id: 27438650,
+        name: "Chiptune, 8bit"
+      }, {
+        id: 28076056,
+        name: "Nintendocore"
+      }, {
+        id: 27721845,
+        name: "Funk"
+      }, {
+        id: 31662376,
+        name: "Disco"
+      }, {
+        id: 59476633,
+        name: "Industrial"
+      }, {
+        id: 36058582,
+        name: "EBM"
+      }, {
+        id: 30597807,
+        name: "Dark Electro"
+      }, {
+        id: 26893663,
+        name: "Noise"
+      }, {
+        id: 51762425,
+        name: "Rhythmic Noise"
+      }, {
+        id: 90164394,
+        name: "Power Electronics"
+      }, {
+        id: 101522128,
+        name: "Techno"
+      }, {
+        id: 47398728,
+        name: "Detroit Techno"
+      }, {
+        id: 55120308,
+        name: "Dub Techno"
+      }, {
+        id: 40079774,
+        name: "House"
+      }, {
+        id: 109696386,
+        name: "Balearic"
+      }, {
+        id: 32465350,
+        name: "UK Garage"
+      }, {
+        id: 86905866,
+        name: "Progressive Electronic"
+      }, {
+        id: 27726199,
+        name: "Trance"
+      }, {
+        id: 26551809,
+        name: "Goatrance & Psytrance"
+      } 
     ]
   }, {
     name: 'Rock',
     groups: [
       {
-        name: 'mathcore',
-        id: 28457642
+        id: 26469953,
+        name: "Rock-n-Roll & Others"
       }, {
-        name: 'chaotic hardcore',
-        id: 37441775
+        id: 33968841,
+        name: "Classic Rock"
+      }, {
+        id: 63873859,
+        name: "Garage Rock"
+      }, {
+        id: 32635229,
+        name: "Progressive Rock"
+      }, {
+        id: 35083039,
+        name: "Krautrock"
+      }, {
+        id: 98512740,
+        name: "Folk Rock"
+      }, {
+        id: 66416196,
+        name: "Country Rock"
+      }, {
+        id: 27449833,
+        name: "Psychobilly"
+      }, {
+        id: 51236599,
+        name: "Psychedelic Rock"
+      }, {
+        id: 27749618,
+        name: "Surf Rock"
+      }, {
+        id: 32704704,
+        name: "Glam"
+      }, {
+        id: 40825255,
+        name: "Alternative Rock"
+      }, {
+        id: 26824676,
+        name: "Grunge"
+      }, {
+        id: 40615042,
+        name: "Noise Rock"
+      }, {
+        id: 41480686,
+        name: "Fuzz Rock"
+      }, {
+        id: 53221782,
+        name: "Occult"
+      }, {
+        id: 51236599,
+        name: "Space Rock"
+      }, {
+        id: 37539781,
+        name: "Neo-prog & Modern Prog"
+      }, {
+        id: 33657028,
+        name: "Mathrock"
+      }, {
+        id: 26071073,
+        name: "Post-rock"
+      }, {
+        id: 35634642,
+        name: "Slowcore / Sadcore"
+      }, {
+        id: 114750970,
+        name: "Stoner"
+      }, {
+        id: 114568690,
+        name: "Sludge"
+      }, {
+        id: 55413494,
+        name: "Indie"
+      }, {
+        id: 84115726,
+        name: "New Wave"
+      }, {
+        id: 50867913,
+        name: "Coldwave"
+      }, {
+        id: 34724138,
+        name: "Darkwave"
+      }, {
+        id: 27667725,
+        name: "Post-punk & Shoegaze"
+      }, {
+        id: 59476633,
+        name: "Industrial"
+      }, {
+        id: 30653307,
+        name: "Gothic Rock"
+      }, {
+        id: 30208116,
+        name: "Deathrock"
+      }, {
+        id: 26736825,
+        name: "Русский Рок"
+      }, {
+        id: 26133751,
+        name: "Experimental Rock"
+      }, {
+        id: 30775286,
+        name: "Avant-garde"
+      }, {
+        id: 26495743,
+        name: "J-Rock"
+      }, {
+        id: 62101761,
+        name: "K-Rock"
+      }, {
+        id: 61842421,
+        name: "Heavy Metal"
+      }, {
+        id: 42138011,
+        name: "Thrash Metal"
+      }, {
+        id: 52654940,
+        name: "Speed Metal"
+      }, {
+        id: 33068179,
+        name: "Industrial Metal"
+      }, {
+        id: 28636332,
+        name: "Djent"
+      }, {
+        id: 26091626,
+        name: "Progressive Metal"
+      }, {
+        id: 26071073,
+        name: "Post Metal"
+      }, {
+        id: 34125866,
+        name: "Drone"
+      }, {
+        id: 114626535,
+        name: "Doom"
+      }, {
+        id: 53221782,
+        name: "Occult"
+      }, {
+        id: 26140698,
+        name: "Black Metal"
+      }, {
+        id: 33970175,
+        name: "Death Metal"
+      }, {
+        id: 27194461,
+        name: "Melodic Death Metal"
+      }, {
+        id: 27526543,
+        name: "Slamming Brutal Death"
+      }, {
+        id: 26549346,
+        name: "Gothic Metal"
+      }, {
+        id: 26939184,
+        name: "Power Metal"
+      }, {
+        id: 28816125,
+        name: "Folk Metal"
+      }, {
+        id: 40952446,
+        name: "Oriental Metal"
+      }, {
+        id: 38229184,
+        name: "Electronic Metal"
+      }, {
+        id: 94076471,
+        name: "Punk"
+      }, {
+        id: 112870000,
+        name: "Pop Punk"
+      }, {
+        id: 74200808,
+        name: "Hardcore Punk"
+      }, {
+        id: 46365798,
+        name: "Thrash Zone"
+      }, {
+        id: 26436403,
+        name: "Grind"
+      }, {
+        id: 43805715,
+        name: "Crust"
+      }, {
+        id: 26929408,
+        name: "Screamo"
+      }, {
+        id: 26364900,
+        name: "Midwest Emo"
+      }, {
+        id: 27774207,
+        name: "Ska"
+      }, {
+        id: 28434538,
+        name: "Celtic Punk"
+      }, {
+        id: 27667725,
+        name: "Post-punk & Shoegaze"
+      }, {
+        id: 74200808,
+        name: "Hardcore Punk"
+      }, {
+        id: 46365798,
+        name: "Thrash Zone"
+      }, {
+        id: 27711883,
+        name: "Melodic Hardcore"
+      }, {
+        id: 37441775,
+        name: "Chaotic Hardcore"
+      }, {
+        id: 28194778,
+        name: "Noisecore"
+      }, {
+        id: 26436403,
+        name: "Grindcore"
+      }, {
+        id: 30321428,
+        name: "Post-hardcore"
+      }, {
+        id: 29537720,
+        name: "Deathcore"
+      }, {
+        id: 25614473,
+        name: "Trancecore"
+      }, {
+        id: 28076056,
+        name: "Nintendocore"
+      }, {
+        id: 29248615,
+        name: "Rapcore"
+      }, {
+        id: 71669171,
+        name: "Horrorcore"
+      }, {
+        id: 28457642,
+        name: "Mathcore"
+      }, {
+        id: 59476633,
+        name: "Industrial"
+      }, {
+        id: 50867913,
+        name: "Coldwave"
+      }, {
+        id: 64525954,
+        name: "Zimna Fala"
+      }, {
+        id: 47530834,
+        name: "Dream Pop"
+      }
+    ]
+  }, {
+    name: "Jazz",
+    groups: [
+      {
+        id: 89501437,
+        name: "Jazz"
+      }, {
+        id: 42606069,
+        name: "Smooth Jazz"
+      }, {
+        id: 47430608,
+        name: "Nu Jazz"
+      }, {
+        id: 33680474,
+        name: "Dark Jazz"
+      }, {
+        id: 50762354,
+        name: "Ethno Jazz"
+      }
+    ]
+  }, {
+    name: "Blues",
+    groups: [
+      {
+        id: 26363764,
+        name: "Blues"
+      }, {
+        id: 31891496,
+        name: "Delta Blues"
+      }, {
+        id: 57321316,
+        name: "Archaic Race Music"
+      }
+    ]
+  }, {
+    name: "Hip-Hop",
+    groups: [
+      {
+        id: 105655882,
+        name: "Hip-Hop"
+      }, {
+        id: 105606548,
+        name: "Cloud Rap"
+      }, {
+        id: 38290461,
+        name: "French Hip-Hop"
+      }, {
+        id: 26992253,
+        name: "Abstract Hip-Hop"
+      }, {
+        id: 27683540,
+        name: "Trip-Hop"
+      }, {
+        id: 71669171,
+        name: "Horrorcore"
+      }, {
+        id: 119425878,
+        name: "Grime"
+      }, {
+        id: 37682149,
+        name: "Wonky"
+      }
+    ]
+  }, {
+    name: "Industrial",
+    groups: [
+      {
+        id: 59476633,
+        name: "Industrial"
+      }, 
+      {
+        id: 33068179,
+        name: "Industrial Metal"
+      }, 
+      {
+        id: 41776457,
+        name: "Martial"
+      }, 
+      {
+        id: 29532853,
+        name: "Dark Folk"
+      }, 
+      {
+        id: 28138343,
+        name: "Dark Ambient"
+      }, 
+      {
+        id: 36058582,
+        name: "EBM"
+      }, 
+      {
+        id: 30597807,
+        name: "Dark Electro"
+      }, 
+      {
+        id: 26893663,
+        name: "Noise"
+      }, 
+      {
+        id: 51762425,
+        name: "Rhythmic Noise"
+      }, 
+      {
+        id: 90164394,
+        name: "Power Electronics"
+      }
+    ]
+  }, {
+    name: "Classical",
+    groups: [
+      {
+        id: 27089831,
+        name: "Art"
+      }, 
+      {
+        id: 44705245,
+        name: "Contemporary Classical"
+      }, 
+      {
+        id: 83776680,
+        name: "Neoclassical"
+      }, 
+      {
+        id: 30775286,
+        name: "Avant-garde"
+      }, 
+      {
+        id: 42252140,
+        name: "Opera"
+      }, 
+      {
+        id: 33961543,
+        name: "Medieval"
+      }, 
+      {
+        id: 62625657,
+        name: "Classicore"
       }, 
     ]
   }, {
-    name: 'Other',
+    name: "Experimental",
     groups: [
       {
+        id: 26133751,
+        name: "Experimental"
+      }, 
+      {
+        id: 30775286,
+        name: "Avant-garde"
+      }, 
+      {
+        id: 101610552,
+        name: "Art Pop"
+      }, 
+      {
+        id: 38765281,
+        name: "Improvisation"
+      }, 
+      {
+        id: 86905866,
+        name: "Progressive Electronic"
+      }, 
+      {
+        id: 53542492,
+        name: "NWW List"
+      }, 
+      {
+        id: 90655362,
+        name: "Spoken Word"
+      }, 
+      {
+        id: 49002559,
+        name: "Field Recording"
+      }, 
+      {
+        id: 32469857,
+        name: "Outsider"
+      }
+    ]
+  }, {
+    name: "Folk",
+    groups: [
+      {
+        id: 26462130,
+        name: "Folk"
+      }, {
+        id: 50313747,
+        name: "Authentic"
+      }, {
+        id: 33961543,
+        name: "Medieval"
+      }, {
+        id: 79107493,
+        name: "Contemporary Folk"
+      }, {
+        id: 41555323,
+        name: "Free Folk"
+      }, {
+        id: 29532853,
+        name: "Dark Folk"
+      }, {
+        id: 36286213,
+        name: "Ritual"
+      }, {
+        id: 97693862,
+        name: "Sacred"
+      }, {
+        id: 66416196,
+        name: "Country"
+      }, {
+        id: 42579569,
+        name: "Tramp Music"
+      }, {
+        id: 30094887,
+        name: "Riot Folk"
+      }, {
+        id: 98512740,
+        name: "Folk Rock"
+      }, {
+        id: 28816125,
+        name: "Folk Metal"
+      }
+    ]
+  }, {
+    name: 'World',
+    groups: [
+      {
+        id: 33863243,
+        name: "Ethnic Eclectic"
+      }, {
+        id: 26843772,
+        name: "Balkan, Gypsy, Jewish etc"
+      }, {
+        id: 35579396,
+        name: "Celtic"
+      }, {
+        id: 32330772,
+        name: "Afrobeat"
+      }, {
+        id: 34361618,
+        name: "Throatsinging"
+      }, {
+        id: 34794486,
+        name: "Tango"
+      }, {
+        id: 40952446,
+        name: "Oriental Metal"
+      }
+    ]
+  }, {
+    name: "Музыка Разных Стран",
+    groups: [
+      {
+        id: 32714500,
+        name: "USSR Music"
+      }, {
+        id: 26736825,
+        name: "Русский Рок"
+      }, {
+        id: 26845423,
+        name: "Red Sons"
+      }, {
+        id: 55904478,
+        name: "Украина"
+      }, {
+        id: 101393585,
+        name: "Эстония"
+      }, {
+        id: 62101761,
+        name: "Korean"
+      }, {
+        id: 26513373,
+        name: "J-Pop"
+      }, {
+        id: 26495743,
+        name: "Japan"
+      }, {
+        id: 33905939,
+        name: "India"
+      }, {
+        id: 31531420,
+        name: "Musica Brasileira"
+      }, {
+        id: 44704113,
+        name: "Iceland"
+      }, {
+        id: 33699684,
+        name: "French Chanson"
+      }, {
+        id: 91587238,
+        name: "Russian Chanson"
+      }
+    ]
+  }, {
+    name: "Other",
+    groups: [
+      {
+        id: 40611023,
+        name: "Swing"
+      }, {
+        id: 29179819,
+        name: "Electroswing"
+      }, {
+        id: 113680198,
+        name: "Reggae"
+      }, {
+        id: 31540482,
+        name: "Acoustic"
+      }, 
+      {
+        id: 45063936,
+        name: "A Cappella"
+      }, 
+      {
+        id: 38148576,
+        name: "Cover"
+      }, 
+      {
+        id: 27344778,
+        name: "Original Soundtrack"
+      }, 
+      {
+        id: 28763462,
+        name: "Kircore"
+      }, 
+      {
+        id: 33831339,
+        name: "Dark Cabaret"
+      }, 
+      {
+        id: 33699684,
+        name: "French Chanson"
+      }, 
+      {
+        id: 91587238,
+        name: "Russian Chanson"
+      }, 
+      {
+        id: 97693862,
+        name: "Sacred"
+      }
+    ]
+  }, {
+    name: "Other Communities",
+    foreign: true,
+    groups: [
+      {
+        name: 'Радио Бога',
+        id: 34049481,
+      }, {
+        name: 'Σ.G.O.',
+        id: 29910855,
+      }, {
+        name: 'atmospheric bass music',
+        id: 25247340,
+      }, {
+        name: 'psychedelic',
+        id: 29318096,
+      }, {
+        name: 'EMDub',
+        id: 29380734,
+      }, {
+        name: 'Stupid Corner',
+        id: 12024126,
+      }, {
+        name: '7parallel',
+        id: 57840926,
+      }, {
+        name: 'АБСТРАСЕНЦИЯ',
+        id: 42932507,
+      }, {
+        name: 'UKfire',
+        id: 25993158,
+      }, {
+        name: 'river of noise',
+        id: 40748081,
+      }, {
+        name: 'SLUDGE & STONER',
+        id: 5857448
+      }, {
+        name: 'Psychill Infinity relax & meditative',
+        id: 17604357
+      }, {
+        name: 'W▲V Ξ N▲T (wavesnat)',
+        id: 57612438
+      }, {
         name: 'lucidity',
-        id: 36976241
+        id: 36976241,
       }
     ]
   }
 ]
 
 
-const groupsById = genres.reduce((groups, genre) => {
-  genre.groups.forEach((group) => groups[group.id] = group)
+genres.byId = genres.reduce((groups, genre) => {
+  genre.groups.forEach((group) => {
+    if (genre.foreign)
+      group.foreign = true
+    groups[group.id] = group
+  })
   return groups
 }, {})
-
-genres.byId = function (id) {
-  return groupsById[id]
-}
 
 export default genres
