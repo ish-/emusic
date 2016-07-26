4c7a81d5b751ae86848fda1d16baa5ff0abeb31d
ISh- <imwaves@gmail.com>
2016-07-26 16:16:18 +0300

    Added: VK standalone mode

--- src/services/vk.js
+++ src/services/vk.js
@@ -4,6 +4,7 @@ import _ from 'utils'
 import Group from 'services/vk.group'
 import Audio from 'services/vk.audio'
 import Storage from 'services/vk.storage'
+import {options} from 'services/vk.request'
 
 const API_VERSION = '5.52'
 
@@ -14,13 +15,57 @@ function initFrameMode () {
   })
 }
 
+function getStorageInfo () {
+  var info = localStorage.getItem('vk--auth')
+  if (!info)
+    return false
+  try {
+    info = JSON.parse(info)
+  } catch (e) {
+    return false
+  }
+  return info
+}
+
 var vk = {
+  standalone: !window.name,
+
+  auth () {
+    location.href = 'https://oauth.vk.com/authorize?client_id=5499210&redirect_uri='+encodeURIComponent(location.href)+'&display=popup&response_type=token&scope=audio';
+  },
+
   inited: (() => {
-    if (window.name)
+    if (window.name) {
+      options.frameMode = true
       return _.addScript("https://vkontakte.ru/js/api/xd_connection.js")
         .then(initFrameMode)
-  })(),
+    } else {
+      return new Promise(function (resolve, reject) {
+        var info = getStorageInfo()
+        if (!info) {
+          let hash = location.hash.substr(1)
+          if (!hash)
+            return reject()
 
+          info = hash.split('&').reduce((hash, prop) => {
+            var [key, value] = prop.split('=')
+            hash[_.camelCase(key)] = value
+            return hash
+          }, {})
+          
+          if (!info || !info.userId)
+            return reject()
+
+          location.hash = ''
+          localStorage.setItem('vk--auth', JSON.stringify(info))
+        }
+
+
+        options.accessToken = info.accessToken
+        resolve()
+      })
+    }
+  })(),
   // props: ((str) => {
   //   return str.substr(1).split('&').reduce((mem, prop) => {
   //     var [key, value] = prop.split('=')
--- src/services/vk.request.js
+++ src/services/vk.request.js
@@ -1,13 +1,83 @@
+const STORAGE_NS = 'vk--'
+const API_URL = 'https://api.vk.com/method/'
+
 export default function request (method, opts) {
+  if (options.frameMode)
+    return _XDMRequest(method, opts)
+
+  if (standaloneMethodsOverride.hasOwnProperty(method))
+    return standaloneMethodsOverride[method](opts)
+  return $jsonp(method, opts)
+}
+
+export var options = {
+  frameMode: false,
+  accessToken: null,
+}
+
+function _XDMRequest (method, opts) {
   return new Promise((resolve, reject) => {
     VK.api(method, opts, function (d) {
       d = d.hasOwnProperty('response') ? d.response : d
-      if (process.env.NODE_ENV === 'development') {
-        console.groupCollapsed('API request: ' + method)
-        console.log('API request: ', opts, d)
-        console.groupEnd()
-      }
+      logRequest(method, opts, d)
+      if (d && d.error)
+        return reject(d.error)
       resolve(d)
-    })
+    }, reject)
   })
+}
+
+const standaloneMethodsOverride = {
+  ['storage.get'] ({keys}) {
+    return Promise.resolve(
+      keys.map((key) => {
+        return {
+          key,
+          value: localStorage.getItem(STORAGE_NS + key)
+        }
+      })
+    )
+  },
+  ['storage.set'] ({key, value}) {
+    return Promise.resolve(
+      localStorage.setItem(STORAGE_NS + key, value)
+    )
+  }
+}
+
+var _requestJsonpIndex = 0
+window.__request_jsonp = {}
+function $jsonp (method, opts) {
+  _requestJsonpIndex++
+  return new Promise ((resolve, reject) => {
+    var requestIndex = _requestJsonpIndex
+    var s = document.createElement('script')
+    s.async = true
+    var query = Object.keys(opts).map((name) => {
+      return `${name}=${encodeURIComponent(opts[name])}`
+    }).join('&')
+    s.src = `${API_URL}${method}?${query}&access_token=${options.accessToken}&callback=__request_jsonp[${requestIndex}]&v=5.52`
+    window.__request_jsonp[requestIndex] = (d) => {
+      d = d.hasOwnProperty('response') ? d.response : d
+      logRequest(method, opts, d)
+      if (d && d.error) {
+        reject(d.error)
+      } else {
+        resolve(d)
+      }
+      delete window.__request_jsonp[requestIndex]
+      document.body.removeChild(s)
+    }
+    s.onerror = reject
+    document.body.appendChild(s)
+  });
+}
+
+const consoleGroupMethod = console.groupCollapsed ? console.groupCollapsed.bind(console) : console.group.bind(console)
+function logRequest (method, opts, d) {
+  if (process.env.NODE_ENV === 'development') {
+    consoleGroupMethod('API request: ' + method)
+    console.log('API request: ', opts, d)
+    console.groupEnd()
+  }
 }
