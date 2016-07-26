b19134bc329408f780180ecd8eeafd28462defbc
ISh- <imwaves@gmail.com>
2016-07-26 16:00:17 +0300

    Added: Shared watch selected group and modifies document.title

--- src/services/shared.js
+++ src/services/shared.js
@@ -1,6 +1,18 @@
+import VK from 'services/vk'
+
+const EMUSIC_DOCUMENT_TITLE = 'E:\\music\\'
+
 var Shared = new Vue({
   data: {
-    group: null
+    group: null,
+  },
+  watch: {
+    group (group) {
+      if (group)
+        document.title = (group.foreign ? '' : EMUSIC_DOCUMENT_TITLE) + group.name
+      else
+        document.title = EMUSIC_DOCUMENT_TITLE
+    }
   }
 })
 
